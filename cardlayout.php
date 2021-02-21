ae7f7ca6447b908ded43b549544809b6d37e3c56
enstrom480b <64636784+enstrom480b@users.noreply.github.com>
2021-02-21 14:31:00 -0600

    Add files via upload
    card-layout-bootstrap

---
+++ cardlayout.php
@@ -0,0 +1,97 @@
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<link rel="stylesheet" href="gallery.css">
+</head>
+<body>
+<section class="facilities">
+<div class="container">
+<div class="title">
+<h1>facilities</h1>
+<p>on the gym floor or in the swimming pool</p>
+</div>
+<div class="row">
+<div class="col-md-3 m-0 p-1 ">
+<div class="card text-center">
+<img src="./images/5.jpg" class="card-img-top">
+<div class="card-body">
+<h5 class="card-title">Gym Floor</h5>
+<P class="card-text mb-1" >some quick example</P>
+<a href="#">CHECK GYM</a>
+</div>
+</div>
+</div>
+
+<div class="col-md-3 m-0 p-1 col-sm-3">
+<div class="card text-center">
+<img src="./images/5.jpg" class="card-img-top">
+<div class="card-body">
+<h5 class="card-title">Gym Floor</h5>
+<P class="card-text mb-1" >some quick example</P>
+<a href="#">CHECK GYM</a>
+</div>
+</div>
+</div>
+<div class="col-md-3 m-0 p-1 col-sm-3">
+<div class="card text-center">
+<img src="./images/5.jpg" class="card-img-top">
+<div class="card-body">
+<h5 class="card-title">Gym Floor</h5>
+<P class="card-text mb-1" >some quick example</P>
+<a href="#">CHECK GYM</a>
+</div>
+</div>
+</div>
+<div class="col-md-3 m-0 p-1 col-sm-3">
+<div class="card text-center">
+<img src="./images/5.jpg" class="card-img-top">
+<div class="card-body">
+<h5 class="card-title">Gym Floor</h5>
+<P class="card-text mb-1" >some quick example</P>
+<a href="#">CHECK GYM</a>
+</div>
+</div>
+</div>
+</div>
+</section>
+</body>
+
+<style>
+*{
+margin:0;
+padding:0;	
+}
+.facilities{
+background-color:#dbe5e6;
+padding:30px 0px;	
+}
+.facilities>h1{
+font-weight:900;
+font-size:34px;
+padding-bottom:5px;	
+}
+.title{
+text-align:center;
+padding:25px 0px;	
+}
+.title p{
+	font-size:18px;
+}
+.card-body{
+	box-shadow:0 0 20px 7px rgba(0,0,0,0.8);
+}
+
+.card-body a{
+	color:#0c7f90;
+	text-decoration:none;
+	font-size:15px;
+	margin-top:auto;
+	
+}
+
+
+
+</style>
